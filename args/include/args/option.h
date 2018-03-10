fd5f52dacad81bab8b6068e05c93638387668232
PaulVisscher <paul.pev@hotmail.com>
2018-03-10 11:22:24 +0100

    Fix shorthand types

--- args/include/args/option.h
+++ args/include/args/option.h
@@ -54,41 +54,41 @@ public:
         }
     };
 
-    static Type<bool> Boolean;
-    static Type<uint32_t> U32;
-    static Type<uint64_t> U64;
-    static Type<int32_t> S32;
-    static Type<int64_t> S64;
-    static Type<float> F32;
-    static Type<double> F64;
-    static Type<std::string> String;
+    static std::shared_ptr<cxxopts::Value> Boolean() { return Type<bool>()();};
+    static std::shared_ptr<cxxopts::Value> U32() { return Type<uint32_t>()();};
+    static std::shared_ptr<cxxopts::Value> U64() { return Type<uint64_t>()();};
+    static std::shared_ptr<cxxopts::Value> S32() { return Type<int32_t>()();};
+    static std::shared_ptr<cxxopts::Value> S64() { return Type<int64_t>()();};
+    static std::shared_ptr<cxxopts::Value> F32() { return Type<float>()();};
+    static std::shared_ptr<cxxopts::Value> F64() { return Type<double>()();};
+    static std::shared_ptr<cxxopts::Value> String() { return Type<std::string>()();};
 
-    static Type<std::vector<bool>> BooleanList;
-    static Type<std::vector<uint32_t>> U32List;
-    static Type<std::vector<uint64_t>> U64List;
-    static Type<std::vector<int32_t>> S32List;
-    static Type<std::vector<int64_t>> S64List;
-    static Type<std::vector<float>> F32List;
-    static Type<std::vector<double>> F64List;
-    static Type<std::vector<std::string>> StringList;
+    static std::shared_ptr<cxxopts::Value> BooleanList() { return Type<std::vector<bool>>()();};
+    static std::shared_ptr<cxxopts::Value> U32List() { return Type<std::vector<uint32_t>>()();};
+    static std::shared_ptr<cxxopts::Value> U64List() { return Type<std::vector<uint64_t>>()();};
+    static std::shared_ptr<cxxopts::Value> S32List() { return Type<std::vector<int32_t>>()();};
+    static std::shared_ptr<cxxopts::Value> S64List() { return Type<std::vector<int64_t>>()();};
+    static std::shared_ptr<cxxopts::Value> F32List() { return Type<std::vector<float>>()();};
+    static std::shared_ptr<cxxopts::Value> F64List() { return Type<std::vector<double>>()();};
+    static std::shared_ptr<cxxopts::Value> StringList() { return Type<std::vector<std::string>>()();};
 
-    static Type<std::optional<bool>> OptionalBoolean;
-    static Type<std::optional<uint32_t>> OptionalU32;
-    static Type<std::optional<uint64_t>> OptionalU64;
-    static Type<std::optional<int32_t>> OptionalS32;
-    static Type<std::optional<int64_t>> OptionalS64;
-    static Type<std::optional<float>> OptionalF32;
-    static Type<std::optional<double>> OptionalF64;
-    static Type<std::optional<std::string>> OptionalString;
+    static std::shared_ptr<cxxopts::Value> OptionalBoolean() { return Type<std::optional<bool>>()();};
+    static std::shared_ptr<cxxopts::Value> OptionalU32() { return Type<std::optional<uint32_t>>()();};
+    static std::shared_ptr<cxxopts::Value> OptionalU64() { return Type<std::optional<uint64_t>>()();};
+    static std::shared_ptr<cxxopts::Value> OptionalS32() { return Type<std::optional<int32_t>>()();};
+    static std::shared_ptr<cxxopts::Value> OptionalS64() { return Type<std::optional<int64_t>>()();};
+    static std::shared_ptr<cxxopts::Value> OptionalF32() { return Type<std::optional<float>>()();};
+    static std::shared_ptr<cxxopts::Value> OptionalF64() { return Type<std::optional<double>>()();};
+    static std::shared_ptr<cxxopts::Value> OptionalString() { return Type<std::optional<std::string>>()();};
 
-    static Type<std::optional<std::vector<bool>>> OptionalBooleanList;
-    static Type<std::optional<std::vector<uint32_t>>> OptionalU32List;
-    static Type<std::optional<std::vector<uint64_t>>> OptionalU64List;
-    static Type<std::optional<std::vector<int32_t>>> OptionalS32List;
-    static Type<std::optional<std::vector<int64_t>>> OptionalS64List;
-    static Type<std::optional<std::vector<float>>> OptionalF32List;
-    static Type<std::optional<std::vector<double>>> OptionalF64List;
-    static Type<std::optional<std::vector<std::string>>> OptionalStringList;
+    static std::shared_ptr<cxxopts::Value> OptionalBooleanList() { return Type<std::optional<std::vector<bool>>>()();};
+    static std::shared_ptr<cxxopts::Value> OptionalU32List() { return Type<std::optional<std::vector<uint32_t>>>()();};
+    static std::shared_ptr<cxxopts::Value> OptionalU64List() { return Type<std::optional<std::vector<uint64_t>>>()();};
+    static std::shared_ptr<cxxopts::Value> OptionalS32List() { return Type<std::optional<std::vector<int32_t>>>()();};
+    static std::shared_ptr<cxxopts::Value> OptionalS64List() { return Type<std::optional<std::vector<int64_t>>>()();};
+    static std::shared_ptr<cxxopts::Value> OptionalF32List() { return Type<std::optional<std::vector<float>>>()();};
+    static std::shared_ptr<cxxopts::Value> OptionalF64List() { return Type<std::optional<std::vector<double>>>()();};
+    static std::shared_ptr<cxxopts::Value> OptionalStringList() { return Type<std::optional<std::vector<std::string>>>()();};
 
     using ArgumentName = std::variant<std::string_view, std::pair<std::string_view, std::string_view>>;
 
